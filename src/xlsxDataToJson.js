7320985d34e6171ca3ea75234618166e70ac4586
Vasidovi <vasido@gmail.com>
2019-01-14 00:02:20 +0200

    retrieved  info about customers and suppliers from credentials and sorted invoices into purchases and sales

--- src/xlsxDataToJson.js
+++ src/xlsxDataToJson.js
@@ -1,34 +1,93 @@
 const XLSX = require('xlsx');
 const config = require('config');
 const dateFormat = require('dateformat');
-const getJson =require('./jsonToTemplate').getJson;
+const getJson = require('./jsonToTemplate').getJson;
 
-const genrateJson = function (path, startDate, endDate) {
+const configuration = config.get("Workbook");
+
+function getInvoicePartner(invoice, credentials) {
+    const customer = {};
+    customer.id = invoice.partnerId;
+   
+    const cred = credentials.find(c => c.id === customer.id);
+    if (cred.code.startsWith("LT")) {
+        customer.VATregistrationNumber = cred.code;
+    } else {
+        customer.registrationNumber = cred.code;
+    }
+    customer.name = cred.name;
+
+    return customer;
+}
+
+function genrateJson(path, startDate, endDate) {
     const workbook = XLSX.readFile(path);
-    const sheets = config.get('Workbook.sheets');
+    const sheets = configuration["sheets"];
     const credentialsSheet = workbook.Sheets[sheets.credentials];
     const credentials = getCredentials(credentialsSheet);
-    
+
     // invoicesSheets =[];
-    const invoicesMetadata =config.get("Workbook.invoices");
+    // configuration.invoices === configuration["invoices"];
+    const invoicesMetadata = configuration.invoices;
     let invoices = [];
     invoicesMetadata.forEach((metadata) => {
 
         const worksheet = workbook.Sheets[metadata.sheetName];
-        const currentInvoices = getInvoices(worksheet,startDate,endDate, metadata);
+        const currentInvoices = getInvoices(worksheet, startDate, endDate, metadata);
         invoices = invoices.concat(currentInvoices);
     })
-    
-    const configuration = config.get("Workbook");
-    const json = getJson({configuration,credentials,invoices,startDate,endDate});
+
+    const purchaseInvoices = invoices.filter(invoice =>
+        !invoice["invoiceNo"].startsWith(configuration.info.invoicePrefix)
+    );
+    const salesInvoices = invoices.filter(invoice =>
+        invoice["invoiceNo"].startsWith(configuration.info.invoicePrefix)
+    );
+
+    const customers = [];
+
+    purchaseInvoices.forEach(invoice => {
+
+         //  if there is a customer with this id in customers []
+         // return works like continue inside forEach loop
+        if (customers.some(c => c.id === invoice.partnerId))
+        return;
+        
+        const customer = getInvoicePartner(invoice, credentials);
+        
+       
+        customers.push(customer);
+    });
+
+    const suppliers = [];
+
+    salesInvoices.forEach(invoice => {
+
+         if (suppliers.some(s => s.id === invoice.partnerId))
+         return;
+
+        const supplier = getInvoicePartner(invoice, credentials);
+        suppliers.push(supplier);
+    });
+
+    const json = getJson({
+        configuration,
+        purchaseInvoices,
+        suppliers,
+        salesInvoices,
+        customers,
+        startDate,
+        endDate
+    });
+
+    console.log(customers);
 
     return json;
 };
 
-
 const getCellOrEmpty = function (worksheet, address, field) {
     field = field || "v";
-    return worksheet[address] ? worksheet[address][field] : "";
+    return worksheet[address] ? "" + worksheet[address][field] : "";
 
 }
 
--- test/xlsxDataToJson.test.js
+++ test/xlsxDataToJson.test.js
@@ -1,26 +1,38 @@
 const expect = require('chai').expect;
 const fs = require('fs');
 const xlsxDataToJson = require('../src/xlsxDataToJson.js');
-
+const sinon = require('sinon');
 
 
 describe('xlsxDataToJson', function () {
-	it('should generate json from xlsx data', function () {
 
-		const jsonOutputExample = JSON.parse(fs.readFileSync('test/data/isaf-structure.json', 'utf8'));
+	const jsonOutputExample = JSON.parse(fs.readFileSync('test/data/isaf-structure.json', 'utf8'));
+	const now = new Date(jsonOutputExample.Header.FileDescription.FileDateCreated);
+	let sandbox;
+	let clock;
+	
+	beforeEach(() => {
+		sandbox = sinon.sandbox.create();
+		clock = sinon.useFakeTimers(now.getTime());
+	});
 
+	afterEach(() => {
+		sandbox.restore();
+		clock.restore();
+	});
+
+
+
+	it('should generate json from xlsx data', function () {	
 		const path = "test/data/sample.xlsx";
 		const startDate = new Date(2019, 0, 1);
 		const endDate = new Date(startDate);
 		endDate.setMonth(endDate.getMonth() + 1);
 		endDate.setDate(endDate.getDate() - 1);
 
-	
 		const json = xlsxDataToJson.genrateJson(path, startDate, endDate);
-		 json.Header.FileDescription.FileDateCreated = jsonOutputExample.Header.FileDescription.FileDateCreated;
 
-
-		fs.writeFileSync('test/out/out.json', JSON.stringify(json));
+			fs.writeFileSync('test/out/out.json', JSON.stringify(json));
 		expect(json).to.deep.equal(jsonOutputExample);
 	});
 });
