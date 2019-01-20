863b0e4764a75cbcc7038667d0bbc0947e791599
Vasidovi <vasido@gmail.com>
2019-01-20 19:54:20 +0200

    working on app design, form design completed, autofilling of default dates completed

--- src/app/main.js
+++ src/app/main.js
@@ -23,7 +23,7 @@ function createWindow () {
 	win.loadFile(path.join(__dirname, 'index.html'));
 
 	// Open the DevTools.
-	// win.webContents.openDevTools();
+	win.webContents.openDevTools();
 
 	// Emitted when the window is closed.
 	win.on('closed', () => {
